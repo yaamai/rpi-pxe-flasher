8499b54df7d5fbd7365b0930c52b8a8bd489c0d7
yaamai <ya_amai@yahoo.co.jp>
2020-10-30 00:37:11 +0900

    refactor entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
-echo "ip=dhcp modules=loop,dhcp,network alpine_repo=http://10.101.101.1:8000 apkovl=http://10.101.101.1:8000/overlay.tar.gz" > $PWD/tftpboot/cmdline.txt
+cleanup() {
+  ip addr del dev $interface $server_ip/$server_prefix_len || true
+  ip link set dev $interface down || true
+}
 
-dnsmasq \
-  -d \
-  -R \
-  -z \
-  -i ${PXE_INTF} \
-  -F ${PXE_SUBNET} \
-  --enable-tftp \
-  --tftp-root=$PWD/tftpboot \
-  --pxe-service=0,"Raspberry Pi Boot" &
+main() {
+  local interface=${PXE_INTF:-enp3s0}
+  local server_ip=${PXE_IP:-10.101.101.1}
+  local server_prefix_len=${PXE_IP_LEN:-24}
+  local server_url=${PXE_URL:-http://$server_ip:8000}
+  local subnet=${PXE_SUBNET:-10.101.101.100,10.101.101.150}
 
-cd $PWD/http && python3 -m http.server &
+  trap cleanup EXIT
 
-wait -n
+  ip addr add dev $interface $server_ip/$server_prefix_len || true
+  ip link set dev $interface up || true
+
+  echo "ip=dhcp modules=loop,dhcp,network alpine_repo=$server_url apkovl=$server_url/overlay.tar.gz" > $PWD/tftpboot/cmdline.txt
+
+  dnsmasq \
+    -d \
+    -R \
+    -z \
+    -i $interface \
+    -F $subnet \
+    --enable-tftp \
+    --tftp-root=$PWD/tftpboot \
+    --pxe-service=0,"Raspberry Pi Boot" &
+
+  cd $PWD/http && python3 -m http.server &
+
+  wait -n
+}
+
+main
